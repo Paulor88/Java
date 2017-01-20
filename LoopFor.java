c011d932b03d9d58dd592308c0d1fab062f414fd
PauloR <paulor88@outlook.com>
2017-01-19 22:55:49 -0200

    FOR

---
+++ LoopFor.java
@@ -0,0 +1,21 @@
+public class LoopFor{
+	
+	public static void main(String[] args){
+		
+		
+		int num;
+		
+		for( num = 1; num <= 10; num++){
+			
+			
+			System.out.println(num);
+			
+		}
+		
+		System.out.println("fim do loop.");
+		
+		
+	}
+	
+	
+}

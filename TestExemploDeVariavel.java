3b190441a1148963b24d74e2dbf2c0a383ff2f51
PauloR <paulor88@outlook.com>
2017-02-25 13:09:10 -0300

    Testa exemplo da Variavel local e Global

---
+++ TestExemploDeVariavel.java
@@ -0,0 +1,14 @@
+public class TestExemploDeVariavel{
+
+
+	public static void main(String[] args){
+	
+	
+		ExemploDeVariavel exemplo = new ExemploDeVariavel();
+	
+		exemplo.inicio();
+		
+	
+	}
+
+}
